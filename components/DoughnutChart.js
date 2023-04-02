5efb7546ff902e7bb320fd6f691d9b01d86952d0
odikanwa <odikanwamichael@gmail.com>
2023-04-02 17:58:02 +0100

    doughnutchart modified

--- components/DoughnutChart.js
+++ components/DoughnutChart.js
@@ -18,12 +18,12 @@ export const data = {
       label: "Kiwinative held",
       data: [10000000, 1000000, 3000000, 5000000, 2000000, 3000000],
       backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 99, 132, 0.5)",
+        "rgba(54, 162, 235, 0.5)",
+        "rgba(255, 206, 86, 0.5)",
+        "rgba(75, 192, 192, 0.5)",
+        "rgba(153, 102, 255, 0.5)",
+        "rgba(255, 159, 64, 0.5)",
       ],
       borderColor: [
         "rgba(255, 99, 132, 1)",
@@ -33,14 +33,14 @@ export const data = {
         "rgba(153, 102, 255, 1)",
         "rgba(255, 159, 64, 1)",
       ],
-      borderWidth: 1,
-      space: 2,
-      borderJoinStyle: "round",
-      offset: 2,
+      borderWidth: 2,
+      space: 10,
+      borderJoinStyle: "miter",
+      offset: 10,
     },
   ],
 };
 
 export const DoughnutChart = () => {
-  return <Doughnut data={data} />;
+  return <Doughnut data={data} className="text-darkWhite" />;
 };
