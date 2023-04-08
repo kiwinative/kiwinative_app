dc28473003f2ad7635d2354c5294395b3a8f5033
odikanwa <odikanwamichael@gmail.com>
2023-04-08 14:27:25 +0100

    header modified

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
   const {menuToggle, setMenuToggle} = useGlobalContext();
   return (
     <nav className="h-auto w-full top-0 fixed z-20 bg-darkIndigo">
-      <div className="w-full flex flex-row items-center justify-between pl-2 md:pl-10 py-2 pr-3 md:pr-12">
+      <div className="w-full flex flex-row items-center justify-between pl-2 md:pl-10 pr-3 md:pr-12">
         <div className="inline-flex items-center justify-start">
       <GoKebabVertical className="block md:hidden text-white text-[30px]" onClick={() => {setMenuToggle(!menuToggle)}}/>
       <div className="inline-flex items-center justify-start p-2">
@@ -19,11 +19,11 @@ export default function Header() {
             <Image
               alt="logo"
               src={logo}
-              className="sm:w-[35px] md:w-[45px] w-[55px] sm:h-[35px] md:h-[45px] h-[55px]"
+              className="w-[35px] sm:w-[35px] md:w-[45px] h-[35px] sm:h-[35px] md:h-[45px] "
             />
           </div>
           <div>
-            <h3 className="sm:text-[11px] md:text-[17px] text-[11px] tracking-[0.3px] ml-[-10px] pt-6 font-extrabold text-darkWhite">
+            <h3 className="sm:text-[11px] md:text-[17px] text-[9px] tracking-[0.3px] ml-[-10px] pt-6 font-extrabold text-lightGreen">
               Kiwinative
             </h3>
           </div>
